77067357a82a07e626426744eac24828147eaf84
tirey93 <tirey93@gmail.com>
2025-03-22 18:07:04 +0100

    added simple login form

--- frontend/webschedule-app/src/app/login/login.component.ts
+++ frontend/webschedule-app/src/app/login/login.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators, FormsModule  } from '@angular/forms';
+
+interface LoginForm {
+  username: FormControl<string | null>;
+  password: FormControl<string | null>;
+}
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, ReactiveFormsModule, FormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  formLogin: FormGroup<LoginForm> = new FormGroup({
+    username: new FormControl('', [
+      Validators.required
+    ]),
+    password: new FormControl('', [
+      Validators.required
+    ])
+  })
 
+  onSubmit() {
+    console.log(this.formLogin.controls.username.value, this.formLogin.controls.password.value)
+    throw new Error('Method not implemented.');
+  }
 }

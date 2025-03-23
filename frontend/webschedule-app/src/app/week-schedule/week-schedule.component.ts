f391a0e5e4aa865665e7b50b61f97bad696eb668
tirey93 <tirey93@gmail.com>
2025-03-23 12:06:27 +0100

    get data about user

--- frontend/webschedule-app/src/app/week-schedule/week-schedule.component.ts
+++ frontend/webschedule-app/src/app/week-schedule/week-schedule.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserRepositoryService } from '../services/UserRepository.service';
+import { UserResponse } from '../login/dtos/userResponse';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common'; // Dodaj import
+
 
 @Component({
   selector: 'app-week-schedule',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './week-schedule.component.html',
   styleUrl: './week-schedule.component.scss'
 })
-export class WeekScheduleComponent {
+export class WeekScheduleComponent implements OnInit {
+  constructor(
+    private userRepositoryService: UserRepositoryService) {}
+
+  loggedUser$!: Observable<UserResponse>;
+  ngOnInit(): void {
+    this.loggedUser$ = this.userRepositoryService.getLoggedIn$();
+  }
 
 }

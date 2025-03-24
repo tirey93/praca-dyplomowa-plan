c8e8e43d65eba3b01113870d43e4645127dc9532
tirey93 <tirey93@gmail.com>
2025-03-24 19:06:33 +0100

    added some test

---
+++ backend/WebSchedule.ZTest.Domain/Study/GroupTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using WebSchedule.Domain.Entities.Study;
+
+namespace WebSchedule.ZTest.Domain.Study
+{
+    public class GroupTests
+    {
+        [Fact]
+        public void ToString_ShouldSuccess()
+        {
+            var group = new Group
+            {
+                StartingYear = 2015,
+                Major = new Major { ShortName = "ABC" },
+                StudyLevel = StudyLevel.Bachelor,
+                StudyMode = StudyMode.PartTime
+            };
+
+            var result = group.ToString();
+
+            result.Should().Be("2015NL - ABC");
+        }
+    }
+}

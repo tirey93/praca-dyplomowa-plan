f69c3e386c12d944d974cdc0f1bdcef3f3c28f8d
tirey93 <tirey93@gmail.com>
2025-05-09 19:12:24 +0200

    some front improvements

--- frontend/webschedule-app/src/app/app.component.ts
+++ frontend/webschedule-app/src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+import {MatMenuModule} from '@angular/material/menu';
+import { JwtService } from './services/Jwt.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatToolbarModule, MatIconModule, MatMenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  constructor(private jwtService: JwtService) {}
   title = 'webschedule-app';
+
+  showIcon() : boolean {
+    console.log('showIcon');
+    return this.jwtService.isTokenValid();
+  }
 }

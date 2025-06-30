76abab6022f98261b8a176afd674ab2430ff8620
tirey93 <tirey93@gmail.com>
2025-06-30 20:20:37 +0200

    fix and nice password

--- frontend/webschedule-app/src/app/preferences/preferences.component.ts
+++ frontend/webschedule-app/src/app/preferences/preferences.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, FormGroupDirective, NgForm, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogActions, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatError, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
@@ -9,6 +9,13 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { UserRepositoryService } from '../../services/user/userRepository.service';
 import { SnackBarErrorService } from '../../services/snack-bar-error-service';
 import { CommonModule } from '@angular/common';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    return !!(control &&  control.valid && (control.dirty || control.touched) && form?.hasError('passwordMismatch'));
+  }
+}
 
 @Component({
   selector: 'app-preferences',
@@ -34,15 +41,20 @@ throw new Error('Method not implemented.');
 onNoClick() {
 throw new Error('Method not implemented.');
 }
-  test() {
-    console.log(this.prefForm.hasError('passwordMismatch'));
-  }
   defaultLogin = '';
   defaultDisplayName = '';
+  matcher = new MyErrorStateMatcher();
   prefForm = new FormGroup({
     login: new FormControl(),
     displayName: new FormControl(),
-    newPassword: new FormControl('', [Validators.required, Validators.minLength(8), this.passwordStrengthValidator()]),
+    newPassword: new FormControl('', [
+      Validators.required, 
+      Validators.minLength(8), 
+      this.upperCaseValidator(),
+      this.lowerCaseValidator(),
+      this.numericValidator(),
+      this.specialValidator(),
+    ]),
     repeatPassword: new FormControl('', [Validators.required]),
   }, {validators: [this.passwordMatchValidator]});
   
@@ -65,21 +77,47 @@ throw new Error('Method not implemented.');
     })
   }
 
-  passwordStrengthValidator(): ValidatorFn {
+  upperCaseValidator(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       const value = control.value;
       if (!value) {
         return null;
       }
-
       const hasUpperCase = /[A-Z]/.test(value);
+      return !hasUpperCase ? { passwordUpperCase: true } : null;
+    };
+  }
+
+  lowerCaseValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      if (!value) {
+        return null;
+      }
       const hasLowerCase = /[a-z]/.test(value);
+      return !hasLowerCase ? { passwordLowerCase: true } : null;
+    };
+  }
+
+  numericValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      if (!value) {
+        return null;
+      }
       const hasNumeric = /[0-9]/.test(value);
+      return !hasNumeric ? { passwordNumeric: true } : null;
+    };
+  }
+
+  specialValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      if (!value) {
+        return null;
+      }
       const hasSpecial = /[!@#$%^&*(),.?":{}|<>]/.test(value);
-
-      const passwordValid = hasUpperCase && hasLowerCase && hasNumeric && hasSpecial;
-
-      return !passwordValid ? { passwordStrength: true } : null;
+      return !hasSpecial ? { passwordSpecial: true } : null;
     };
   }
 

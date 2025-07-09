dd1c5bc141e466db3c2de16e3caf7d1f75330d21
tirey93 <tirey93@gmail.com>
2025-07-09 19:47:28 +0200

    added groups to display working

--- frontend/webschedule-app/src/app/main/week-schedule/week-schedule.component.ts
+++ frontend/webschedule-app/src/app/main/week-schedule/week-schedule.component.ts
@@ -1,21 +1,39 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { UserResponse } from '../../../services/user/dtos/userResponse';
-import { Observable } from 'rxjs';
+import { filter, Observable, switchMap } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
 import { AsyncPipe } from '@angular/common';
 import { UserRepositoryService } from '../../../services/user/userRepository.service';
+import { GroupRepositoryService } from '../../../services/group/groupRepository.service';
+import { LoginService } from '../../../services/login.service';
+import { GroupHelper } from '../../../helpers/groupHelper';
 
 @Component({
   selector: 'app-week-schedule',
-  imports: [AsyncPipe, MatButtonModule],
+  imports: [MatButtonModule],
   templateUrl: './week-schedule.component.html',
   styleUrl: './week-schedule.component.scss'
 })
-export class WeekScheduleComponent {
+export class WeekScheduleComponent implements OnInit{
   @Input() groupName?: string;
+  groupsToDisplay: string[] = []
   
   constructor(
-    private userRepositoryService: UserRepositoryService) {
+    private groupRepository: GroupRepositoryService,
+    private loginService: LoginService
+  ) {
       
     }
+  ngOnInit(): void {
+    this.loginService.isLoggedIn$.pipe(
+        filter(isLoggedIn => isLoggedIn && this.groupName == null),
+        switchMap(x => this.groupRepository.getByLoggedIn$())
+      ).subscribe(userGroupResponses => {
+        const groupNames = userGroupResponses.map(x => GroupHelper.groupInfoToString(x))
+        this.groupsToDisplay.push(...groupNames)
+      });
+
+      if (this.groupName)
+        this.groupsToDisplay.push(this.groupName);
+  }
 }

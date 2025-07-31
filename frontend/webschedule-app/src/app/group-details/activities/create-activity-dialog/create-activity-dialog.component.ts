b447c4e25dda76335431f30d4bb5d3c07f3f438c
tirey93 <tirey93@gmail.com>
2025-07-31 07:39:27 +0200

    added dialog description

--- frontend/webschedule-app/src/app/group-details/activities/create-activity-dialog/create-activity-dialog.component.ts
+++ frontend/webschedule-app/src/app/group-details/activities/create-activity-dialog/create-activity-dialog.component.ts
@@ -1,14 +1,52 @@
 import { Component, inject } from '@angular/core';
 import { UserGroupResponse } from '../../../../services/userInGroup/dtos/userGroupResponse';
 import { DIALOG_DATA } from '@angular/cdk/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { GroupHelper } from '../../../../helpers/groupHelper';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SelectValue } from '../../../dtos/selectValue';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-create-activity-dialog',
-  imports: [],
+  imports: [
+    MatDialogModule, ReactiveFormsModule, MatTooltipModule, MatButtonModule
+  ],
   templateUrl: './create-activity-dialog.component.html',
   styleUrl: './create-activity-dialog.component.scss'
 })
 export class CreateActivityDialogComponent {
+
   data = inject(DIALOG_DATA);
   userGroup: UserGroupResponse = this.data.userGroup;
+  springSemester: boolean = this.isSpringSemester();
+
+
+  activityForm = new FormGroup({
+    name: new FormControl("", {validators: [Validators.required]}),
+    teacherFullName: new FormControl("", {validators: [Validators.required]}),
+    duration: new FormControl(2, {validators: [Validators.required]}),
+    startingHour: new FormControl<SelectValue | null>(null),
+    sessionNumber: new FormControl<SelectValue | null>(null),
+  });
+
+  constructor(
+    private dialogRef: MatDialogRef<CreateActivityDialogComponent>,
+  ) {
+  }
+
+  onNoClick() {
+    this.dialogRef.close();
+  }
+
+  submit() {
+    throw new Error('Method not implemented.');
+  }
+  getGroupName(userGroup: UserGroupResponse): string { return GroupHelper.groupInfoToString(userGroup.group)}
+
+  private isSpringSemester(): boolean {
+    return false;
+  }
 }
+
